dffc23421fb0bc2ca9772e7b79e9704a64119348
Mary <mariyahaak@gmail.com>
2018-10-31 03:13:39 +0300

    refactor() linter

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
-import generator from './generator';
 import * as commander from 'commander';
+import generator from './generator';
 
 const initGendiff = () => {
-    program
-        .version('1.0.5')
-        .usage('[options] <firstConfig> <secondConfig>')
-        .description('Compares two configuration files and shows a difference.')
-        .option('-f, --format [type]', 'Output format')
-        .action(generator);
+  const program = new commander.Command();
 
-    program.parse(process.argv);
+  program
+    .version('1.0.5')
+    .usage('[options] <firstConfig> <secondConfig>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format')
+    .action(generator);
+
+  program.parse(process.argv);
 };
 
 initGendiff();
--- src/bin/generator.js
+++ src/bin/generator.js
@@ -3,60 +3,62 @@ import * as path from 'path';
 import has from 'lodash/has';
 
 const buildResStr = (arr) => {
-    const propsStr = arr.reduce((acc, item) => (acc + `\n\t${item.state}${item.key}: ${item.val}`), '');
-    return `{${propsStr}\n}`;
+  const propsStr = arr.reduce((acc, item) => `${acc}\n\t${item.state}${item.key}: ${item.val}`, '');
+  return `{${propsStr}\n}`;
 };
 
 
 const parse = (beforeObj, afterObj) => {
-    const diffArr = [];
-    Object.keys(beforeObj).map(function(key){
-        const beforeLineObj = { 'key': key, 'val': beforeObj[key] };
-        //то, что есть в первом: +, - или space
-        if (has(afterObj, key)) {
-            if (afterObj[key] === beforeObj[key]) {
-                beforeLineObj.state = '  ';
-            } else {
-                const afterLineObj = { 'key': key, 'val': afterObj[key] };
-                afterLineObj.state = '+ ';
-                diffArr.push(afterLineObj);
-                beforeLineObj.state = '- ';
-            }
-        } else {
-            beforeLineObj.state = '- ';
-        }
-        diffArr.push(beforeLineObj);
-    });
-    Object.keys(afterObj).map(function(key){
-        if (!has(beforeObj, key)) {
-            const afterLineObj = { 'key': key, 'val': afterObj[key] };
-            afterLineObj.state = '+ ';
-            diffArr.push(afterLineObj);
-        }
-    });
-    return diffArr;
+  const diffArr = [];
+  Object.keys(beforeObj).forEach(function(key){
+    const beforeLineObj = { 'key': key, 'val': beforeObj[key] };
+    if (has(afterObj, key)) {
+      if (afterObj[key] === beforeObj[key]) {
+        beforeLineObj.state = '  ';
+      } else {
+        const afterLineObj = { 'key': key, 'val': afterObj[key] };
+        afterLineObj.state = '+ ';
+        diffArr.push(afterLineObj);
+        beforeLineObj.state = '- ';
+      }
+    } else {
+      beforeLineObj.state = '- ';
+    }
+    diffArr.push(beforeLineObj);
+  });
+  
+  Object.keys(afterObj).forEach(function(key){
+    if (!has(beforeObj, key)) {
+      const afterLineObj = { 'key': key, 'val': afterObj[key] };
+      afterLineObj.state = '+ ';
+      diffArr.push(afterLineObj);
+    }
+  });
+  return diffArr;
 }
 
 
 const readFile = (path) =>
-    fs.readFileSync(path, 'utf8', function (err, data) {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                console.error(err.message);
-            } else {
-                console.error(err);
-            }
-        } else {
-            console.log(data);
-        }
-    });
+  fs.readFileSync(path, 'utf8', function (err, data) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        console.error(err.message);
+      } else {
+        console.error(err);
+      }
+    } else {
+      console.log(data);
+    }
+  });
 
 export default (firstConfig, secondConfig) => {
-    const beforeData = readFile(path.resolve(__dirname, firstConfig));
-    const afterData = readFile(path.resolve(__dirname, secondConfig));
-    const beforeObj = JSON.parse(beforeData);
-    const afterObj = JSON.parse(afterData);
+  const beforeData = readFile(path.resolve(__dirname, firstConfig));
+  const afterData = readFile(path.resolve(__dirname, secondConfig));
+  const beforeObj = JSON.parse(beforeData);
+  const afterObj = JSON.parse(afterData);
 
-    const diffArr = parse(beforeObj, afterObj);
-    return buildResStr(diffArr);
+  const diffArr = parse(beforeObj, afterObj);
+  const res = buildResStr(diffArr);
+  console.log(res);
+  return res;
 };
