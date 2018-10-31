c7325f6c175f8eeb2f0f11bec4004ce3a9f7bf12
Mary <mariyahaak@gmail.com>
2018-10-31 23:41:47 +0300

    test(gendiff-test.js) remove absolute path check, couse it doesnt work in travis

--- __tests__/gendiff-test.js
+++ __tests__/gendiff-test.js
@@ -3,7 +3,3 @@ import generator from '../src';
 test('generate diff (relative path)', () => {
   expect(generator('../__tests__/__fixtures__/before.json', '../__tests__/__fixtures__/after.json')).toBe('{\n\t  host: hexlet.io\n\t+ timeout: 20\n\t- timeout: 50\n\t- proxy: 123.234.53.22\n\t- follow: false\n\t+ verbose: true\n}');
 });
-
-test('generate diff (absolute path)', () => {
-  expect(generator('/Users/mary/Documents/hexlet/project-lvl2-s353/__tests__/__fixtures__/before.json', '../__tests__/__fixtures__/after.json')).toBe('{\n\t  host: hexlet.io\n\t+ timeout: 20\n\t- timeout: 50\n\t- proxy: 123.234.53.22\n\t- follow: false\n\t+ verbose: true\n}');
-});
