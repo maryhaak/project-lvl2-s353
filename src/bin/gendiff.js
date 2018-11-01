0693e17fa0c0ec9560c0c728f6276b8b654cf7c6
Mary <mariyahaak@gmail.com>
2018-11-02 00:37:47 +0300

    refactor(bin) add program output

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,5 +8,7 @@ program
   .usage('[options] <firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
-  .action(generateDiff);
+  .action(function(firstConfig, secondConfig){
+    console.log(generateDiff(firstConfig, secondConfig));
+  });
 program.parse(process.argv);
