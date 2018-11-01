72d403357b8ec82067ef8733e421a1382a7e6072
Mary <mariyahaak@gmail.com>
2018-11-01 15:48:15 +0300

    refactor(index.js) remove file parser to other file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
-import fs from 'fs';
-import path from 'path';
-import yaml from 'js-yaml';
-import parse from './parsers';
+import _ from 'lodash';
+import has from 'lodash/has';
+import parseFile from './parsers';
 
 
-const getFileValue = (config) => {
-  const configPath = path.isAbsolute(config) ? config : path.resolve(__dirname, config);
-  const str = fs.readFileSync(configPath, 'utf8');
-  const type = path.extname(config);
-  return type === '.json' ? JSON.parse(str) : yaml.safeLoad(str);
-};
+const combineKeyObj = (keysArr, beforeObj, afterObj, keysBelongTo) => keysArr.map((key) => {
+  const obj = { key };
+  obj.val = keysBelongTo === 'before' ? beforeObj[key] : afterObj[key];
+  /* detect prefix */
+  if (has(beforeObj, key) && has(afterObj, key)) {
+    if (beforeObj[key] !== afterObj[key]) {
+      obj.prefix = keysBelongTo === 'before' ? '- ' : '+ ';
+    } else {
+      obj.prefix = '  ';
+    }
+  } else if (has(beforeObj, key) && !has(afterObj, key)) {
+    obj.prefix = '- ';
+  } else if (!has(beforeObj, key) && has(afterObj, key)) {
+    obj.prefix = '+ ';
+  }
+  return obj;
+});
 
 
 export default (firstConfig, secondConfig) => {
-  const res = parse(getFileValue(firstConfig), getFileValue(secondConfig));
+  const beforeObj = parseFile(firstConfig);
+  const afterObj = parseFile(secondConfig);
+  const beforeKeysObjArr = combineKeyObj(Object.keys(beforeObj), beforeObj, afterObj, 'before');
+  const afterKeysObjArr = combineKeyObj(Object.keys(afterObj), beforeObj, afterObj, 'after');
+  const diffArr = beforeKeysObjArr.concat(afterKeysObjArr);
+
+  diffArr.sort((a, b) => {
+    if (a.key < b.key) { return -1; }
+    if (a.key > b.key) { return 1; }
+    return 0;
+  });
+  const resArr = _.uniqBy(diffArr, 'val');
+  const propsStr = resArr.reduce((acc, item) => `${acc}\n${item.prefix}${item.key}: ${item.val}`, '');
+  const res = `{${propsStr}\n}\n`;
+
   console.log('RESULT:');
   console.log(res);
   return res;
--- src/parsers.js
+++ src/parsers.js
@@ -1,39 +1,11 @@
-import _ from 'lodash';
-import has from 'lodash/has';
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
 
 
-const combineKeyObj = (keysArr, beforeObj, afterObj, keysBelongTo) => keysArr.map((key) => {
-  const obj = { key };
-  obj.val = keysBelongTo === 'before' ? beforeObj[key] : afterObj[key];
-  /* detect prefix */
-  if (has(beforeObj, key) && has(afterObj, key)) {
-    if (beforeObj[key] !== afterObj[key]) {
-      obj.prefix = keysBelongTo === 'before' ? '- ' : '+ ';
-    } else {
-      obj.prefix = '  ';
-    }
-  } else if (has(beforeObj, key) && !has(afterObj, key)) {
-    obj.prefix = '- ';
-  } else if (!has(beforeObj, key) && has(afterObj, key)) {
-    obj.prefix = '+ ';
-  }
-  return obj;
-});
-
-
-export default (beforeObj, afterObj) => {
-  const beforeKeys = Object.keys(beforeObj);
-  const afterKeys = Object.keys(afterObj);
-  const beforeKeysObjArr = combineKeyObj(beforeKeys, beforeObj, afterObj, 'before');
-  const afterKeysObjArr = combineKeyObj(afterKeys, beforeObj, afterObj, 'after');
-  const diffArr = beforeKeysObjArr.concat(afterKeysObjArr);
-  diffArr.sort((a, b) => {
-    if (a.key < b.key) { return -1; }
-    if (a.key > b.key) { return 1; }
-    return 0;
-  });
-  const resArr = _.uniqBy(diffArr, 'val');
-
-  const propsStr = resArr.reduce((acc, item) => `${acc}\n${item.prefix}${item.key}: ${item.val}`, '');
-  return `{${propsStr}\n}\n`;
+export default (fileName) => {
+  const pathAbsolute = path.isAbsolute(fileName) ? fileName : path.resolve(__dirname, fileName);
+  const str = fs.readFileSync(pathAbsolute, 'utf8');
+  const type = path.extname(fileName);
+  return type === '.json' ? JSON.parse(str) : yaml.safeLoad(str);
 };
