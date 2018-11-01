b76f64b7d0df40b66f5b4b71244b57864db3b8e2
Mary <mariyahaak@gmail.com>
2018-11-01 10:12:54 +0300

    feature(gendiff) add .yml parse

--- __tests__/gendiff-test.js
+++ __tests__/gendiff-test.js
@@ -2,7 +2,12 @@ import * as fs from 'fs';
 import * as path from 'path';
 import generator from '../src';
 
-test('generate diff (relative path)', () => {
-  const expected = fs.readFileSync(path.resolve(__dirname, './__fixtures__/test-diff.txt'), 'utf8');
+test('json test', () => {
+  const expected = fs.readFileSync(path.resolve(__dirname, './__fixtures__/test-diff-json.txt'), 'utf8');
   expect(generator('../__tests__/__fixtures__/before.json', '../__tests__/__fixtures__/after.json')).toBe(expected);
+});
+
+test('yaml test', () => {
+  const expected = fs.readFileSync(path.resolve(__dirname, './__fixtures__/test-diff-yml.txt'), 'utf8');
+  expect(generator('../__tests__/__fixtures__/before.yml', '../__tests__/__fixtures__/after.yml')).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,69 +1,21 @@
 import * as commander from 'commander';
 import * as fs from 'fs';
 import * as path from 'path';
-import _ from 'lodash';
-import has from 'lodash/has';
+import * as yaml from 'js-yaml';
+import parse from './parsers';
 
 
-const combineKeyObj = (keysArr, beforeObj, afterObj, keysBelongTo) => keysArr.map((key) => {
-  const obj = { key };
-  obj.val = keysBelongTo === 'before' ? beforeObj[key] : afterObj[key];
-  /* detect prefix */
-  if (has(beforeObj, key) && has(afterObj, key)) {
-    if (beforeObj[key] !== afterObj[key]) {
-      obj.prefix = keysBelongTo === 'before' ? '- ' : '+ ';
-    } else {
-      obj.prefix = '  ';
-    }
-  } else if (has(beforeObj, key) && !has(afterObj, key)) {
-    obj.prefix = '- ';
-  } else if (!has(beforeObj, key) && has(afterObj, key)) {
-    obj.prefix = '+ ';
-  }
-  return obj;
-});
-
-
-const parse = (beforeObj, afterObj) => {
-  const beforeKeys = Object.keys(beforeObj);
-  const afterKeys = Object.keys(afterObj);
-  const beforeKeysObjArr = combineKeyObj(beforeKeys, beforeObj, afterObj, 'before');
-  const afterKeysObjArr = combineKeyObj(afterKeys, beforeObj, afterObj, 'after');
-  const diffArr = beforeKeysObjArr.concat(afterKeysObjArr);
-  diffArr.sort((a, b) => {
-    if (a.key < b.key) { return -1; }
-    if (a.key > b.key) { return 1; }
-    return 0;
-  });
-  const resArr = _.uniqBy(diffArr, 'val');
-
-  const propsStr = resArr.reduce((acc, item) => `${acc}\n${item.prefix}${item.key}: ${item.val}`, '');
-  return `{${propsStr}\n}\n`;
-};
-
-
-const readFile = (config) => {
+const getFileValue = (config) => {
   const configPath = path.isAbsolute(config) ? config : path.resolve(__dirname, config);
-  return fs.readFileSync(configPath, 'utf8', (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        console.error(err.message);
-      } else {
-        console.error(err);
-      }
-    } else {
-      console.log(data);
-    }
-  });
+  const str = fs.readFileSync(configPath, 'utf8');
+  const type = path.extname(config);
+  return type === '.json' ? JSON.parse(str) : yaml.safeLoad(str);
 };
 
 
 const generator = (firstConfig, secondConfig) => {
-  const beforeData = readFile(firstConfig);
-  const afterData = readFile(secondConfig);
-  const beforeObj = JSON.parse(beforeData);
-  const afterObj = JSON.parse(afterData);
-  const res = parse(beforeObj, afterObj);
+  const res = parse(getFileValue(firstConfig), getFileValue(secondConfig));
+  console.log('RESULT:');
   console.log(res);
   return res;
 };
