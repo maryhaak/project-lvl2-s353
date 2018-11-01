0c705f33aeec35232bb3a79214119ef1f9752ded
Mary <mariyahaak@gmail.com>
2018-11-01 03:16:21 +0300

    refactor(index.js, gendiff-test.js) move test expected data to a file, make general code cleaner

--- __tests__/gendiff-test.js
+++ __tests__/gendiff-test.js
@@ -1,5 +1,8 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import generator from '../src';
 
 test('generate diff (relative path)', () => {
-  expect(generator('../__tests__/__fixtures__/before.json', '../__tests__/__fixtures__/after.json')).toBe('{\n\t  host: hexlet.io\n\t+ timeout: 20\n\t- timeout: 50\n\t- proxy: 123.234.53.22\n\t- follow: false\n\t+ verbose: true\n}');
+  const expected = fs.readFileSync(path.resolve(__dirname, './__fixtures__/test-diff.txt'), 'utf8');
+  expect(generator('../__tests__/__fixtures__/before.json', '../__tests__/__fixtures__/after.json')).toBe(expected);
 });
--- src/index.js
+++ src/index.js
@@ -1,49 +1,49 @@
 import * as commander from 'commander';
 import * as fs from 'fs';
 import * as path from 'path';
+import _ from 'lodash';
 import has from 'lodash/has';
 
-const buildResStr = (arr) => {
-  const propsStr = arr.reduce((acc, item) => `${acc}\n\t${item.state}${item.key}: ${item.val}`, '');
-  return `{${propsStr}\n}`;
-};
+
+const combineKeyObj = (keysArr, beforeObj, afterObj, keysBelongTo) => keysArr.map((key) => {
+  const obj = { key };
+  obj.val = keysBelongTo === 'before' ? beforeObj[key] : afterObj[key];
+  /* detect prefix */
+  if (has(beforeObj, key) && has(afterObj, key)) {
+    if (beforeObj[key] !== afterObj[key]) {
+      obj.prefix = keysBelongTo === 'before' ? '- ' : '+ ';
+    } else {
+      obj.prefix = '  ';
+    }
+  } else if (has(beforeObj, key) && !has(afterObj, key)) {
+    obj.prefix = '- ';
+  } else if (!has(beforeObj, key) && has(afterObj, key)) {
+    obj.prefix = '+ ';
+  }
+  return obj;
+});
 
 
 const parse = (beforeObj, afterObj) => {
-  const diffArr = [];
-  const beforeKeysArr = Object.keys(beforeObj);
-  const afterKeysArr = Object.keys(afterObj);
-  for (let i = 0; i < beforeKeysArr.length; i += 1) {
-    const key = beforeKeysArr[i];
-    const beforeLineObj = { key, val: beforeObj[key] };
-    if (has(afterObj, key)) {
-      if (afterObj[key] === beforeObj[key]) {
-        beforeLineObj.state = '  ';
-      } else {
-        const afterLineObj = { key, val: afterObj[key] };
-        afterLineObj.state = '+ ';
-        diffArr.push(afterLineObj);
-        beforeLineObj.state = '- ';
-      }
-    } else {
-      beforeLineObj.state = '- ';
-    }
-    diffArr.push(beforeLineObj);
-  }
-  for (let i = 0; i < afterKeysArr.length; i += 1) {
-    const key = afterKeysArr[i];
-    if (!has(beforeObj, key)) {
-      const afterLineObj = { key, val: afterObj[key] };
-      afterLineObj.state = '+ ';
-      diffArr.push(afterLineObj);
-    }
-  }
-  return diffArr;
+  const beforeKeys = Object.keys(beforeObj);
+  const afterKeys = Object.keys(afterObj);
+  const beforeKeysObjArr = combineKeyObj(beforeKeys, beforeObj, afterObj, 'before');
+  const afterKeysObjArr = combineKeyObj(afterKeys, beforeObj, afterObj, 'after');
+  const diffArr = beforeKeysObjArr.concat(afterKeysObjArr);
+  diffArr.sort((a, b) => {
+    if (a.key < b.key) { return -1; }
+    if (a.key > b.key) { return 1; }
+    return 0;
+  });
+  const resArr = _.uniqBy(diffArr, 'val');
+
+  const propsStr = resArr.reduce((acc, item) => `${acc}\n${item.prefix}${item.key}: ${item.val}`, '');
+  return `{${propsStr}\n}\n`;
 };
 
 
 const readFile = (config) => {
-  const configPath = path.resolve(__dirname, config);
+  const configPath = path.isAbsolute(config) ? config : path.resolve(__dirname, config);
   return fs.readFileSync(configPath, 'utf8', (err, data) => {
     if (err) {
       if (err.code === 'ENOENT') {
@@ -63,9 +63,7 @@ const generator = (firstConfig, secondConfig) => {
   const afterData = readFile(secondConfig);
   const beforeObj = JSON.parse(beforeData);
   const afterObj = JSON.parse(afterData);
-
-  const diffArr = parse(beforeObj, afterObj);
-  const res = buildResStr(diffArr);
+  const res = parse(beforeObj, afterObj);
   console.log(res);
   return res;
 };
@@ -73,14 +71,12 @@ const generator = (firstConfig, secondConfig) => {
 
 export const initGendiff = () => {
   const program = new commander.Command();
-
   program
     .version('1.0.5')
     .usage('[options] <firstConfig> <secondConfig>')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'Output format')
     .action(generator);
-
   program.parse(process.argv);
 };
 
