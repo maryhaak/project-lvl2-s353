a389b99903a8562a43441aec8fcb5865947a07ee
Mary <mariyahaak@gmail.com>
2018-11-04 03:36:57 +0300

    feature(gendiff) parse nested keys, add parse and render separted functions

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 [шаг #2](https://asciinema.org/a/XBC1fyDVCW5bcymVVyQSyBdfR)  
 [шаг #3](https://asciinema.org/a/T2xzFgcSQddxoXZxL14dVz7Pq)  
 [шаг #4](https://asciinema.org/a/eF5mSLYKVUQ3psDIPFJwuB7PS)  
+[шаг #5](https://asciinema.org/a/ouZE8KX4zrt1tqGdsptvOoEKG)
--- src/index.js
+++ src/index.js
@@ -8,53 +8,83 @@ const prefixes = {
   added: '+ ',
   removed: '- ',
   unchanged: '  ',
+  tab: '    ',
+  indent: '  ',
 };
 
 
-const render = (data) => {
-  const getValStr = value => (_.isObject(value) ? render(value) : value);
+const stringify = (obj, indent) => Object.keys(obj).reduce((acc, key) => `${acc}${indent}${prefixes.indent}${key}: ${obj[key]}\n`, '');
 
-  const keysStr = data.reduce((acc, item) => {
-    const {
-      key,
-      status,
-    } = item;
 
-    const keyDiff = status === 'changed'
-      ? `${prefixes.removed}${key}: ${getValStr(item.value1)}\n${prefixes.added}${key}: ${getValStr(item.value2)}\n`
-      : `${prefixes[status]}${key}: ${getValStr(item.value)}\n`;
+const render = (data, depth = 0) => {
+  const getValStr = value => (_.isObject(value) ? render(value, depth + 1) : value);
+  const indent = prefixes.tab.repeat(depth);
 
-    return `${acc}${keyDiff}`;
-  }, '');
-  return `{\n${keysStr}}\n`;
+  const keysStr = _.isArray(data)
+    ? data.reduce((acc, item) => {
+      const {
+        key,
+        status,
+      } = item;
+
+      const keyDiff = status === 'changed'
+        ? `${indent}${prefixes.removed}${key}: ${getValStr(item.val1)}\n${indent}${prefixes.added}${key}: ${getValStr(item.val2)}\n`
+        : `${indent}${status ? prefixes[status] : prefixes.indent}${key}: ${getValStr(item.value)}\n`;
+
+      return `${acc}${keyDiff}`;
+    }, '')
+    : stringify(data, indent);
+
+  const closingBracketIndent = depth > 0 ? `${prefixes.indent}${prefixes.tab.repeat(depth - 1)}` : '';
+  return `{\n${keysStr}${closingBracketIndent}}`;
 };
+
+
+const diffStatuses = [
+  {
+    name: 'unchanged',
+    check: (hasFirst, hasSecond, v1, v2) => (v1 === v2 || (_.isObject(v1) && _.isObject(v2))),
+  },
+  {
+    name: 'removed',
+    check: (hasFirst, hasSecond) => !hasSecond && hasFirst,
+  },
+  {
+    name: 'added',
+    check: (hasFirst, hasSecond) => hasSecond && !hasFirst,
+  },
+  {
+    name: 'changed',
+    check: (hasFirst, hasSecond, v1, v2) => (hasFirst && hasSecond && (v1 !== v2)),
+  },
+];
 
 
 const parse = (obj1, obj2) => {
   const keysArr = _.uniq(Object.keys(obj1).concat(Object.keys(obj2)));
+
   const keysDiff = keysArr.map((key) => {
     const hasFirst = has(obj1, key);
     const hasSecond = has(obj2, key);
-    const value1 = obj1[key];
-    const value2 = obj2[key];
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+    const status = diffStatuses.find(({ check }) => check(hasFirst, hasSecond, val1, val2)).name;
 
-    if (!hasSecond || !hasFirst) {
-      const status = hasFirst ? 'removed' : 'added';
-      const value = hasFirst ? value1 : value2;
+    if (status === 'added' || status === 'removed') {
+      const value = status === 'removed' ? val1 : val2;
       return { key, status, value };
     }
 
-    if (value1 === value2 || (_.isObject(value1) && _.isObject(value2))) {
-      const status = 'unchanged';
-      const value = (value1 === value2) ? value1 : parse(value1, value2);
+    if (status === 'unchanged') {
+      const value = (val1 === val2) ? val1 : parse(val1, val2);
       return { key, status, value };
     }
 
-    const status = 'changed';
     return {
-      key, status, value1, value2,
+      key, status, val1, val2,
     };
   });
+
   return keysDiff;
 };
 
@@ -62,5 +92,5 @@ const parse = (obj1, obj2) => {
 export default (path1, path2) => {
   const obj1 = getFileDataAsObj(fs.readFileSync(path1, 'utf8'), path.extname(path1));
   const obj2 = getFileDataAsObj(fs.readFileSync(path2, 'utf8'), path.extname(path2));
-  return render(parse(obj1, obj2));
+  return `${render(parse(obj1, obj2))}\n`;
 };
